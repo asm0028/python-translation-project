426d86561ea6f9304aa2900759e49e975e58070e
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-04-07 09:27:15 -0500

    Changed the stop codon to only be UAA and seems to work for cases other than when the start and stop codon are custom

--- find_orf.py
+++ find_orf.py
@@ -207,7 +207,7 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUG.*?(UAA)|(UAG)|(UGA)\Z'
+    orf_pattern_str = r'AUG([AUGC]{3})*UAA'
     ##########################################################################
 
     # Create the regular expression object
